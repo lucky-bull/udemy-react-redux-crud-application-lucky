e20bd9dd92885fe07f74368a7b9661d614bbe1f5
Nana Matsumoto <nanmatsu@yahoo-corp.jp>
2020-08-11 16:21:04 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-function App() {
-  return(
-    <React.Fragment>
-      <label htmlFor="bar">aaa</label>
-      <input type="text" onChange={() => {console.log('change')}}></input>
-    </React.Fragment>
-  )
+// function App() {
+//   return(
+//     <React.Fragment>
+//       <label htmlFor="bar">aaa</label>
+//       <input type="text" onChange={() => {console.log('change')}}></input>
+//     </React.Fragment>
+//   )
+// }
+
+const App = () => {
+  return (<div>Hi!</div>)
 }
 
 export default App;

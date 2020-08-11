14a380d2d5bef14ca9a9be1b78e3cae9c8f97bf4
Nana Matsumoto <nanmatsu@yahoo-corp.jp>
2020-08-11 16:38:04 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // function App() {
 //   return(
@@ -10,7 +10,33 @@ import React from 'react';
 // }
 
 const App = () => {
-  return (<div>Hi!</div>)
-}
+  // 一括で変数で管理して、mapで回しながら表示
+  // ユニークキー どのdomが変更になったのかを管理してるから。必要最低限のdomの範囲をみている。
+  // index を入れるのが最良
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "miki", age: 27 },
+    { name: "NoName" },
+  ];
+  return (
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} key={index} />;
+      })}
+    </div>
+  );
+};
+
+const User = (props) => {
+  return (
+    <div>
+      Hi, I'm {props.name}, and {props.age} years old.
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1,
+};
 
 export default App;
